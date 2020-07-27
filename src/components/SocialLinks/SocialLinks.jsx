b6d7f34a939e57be9af7e42906dc57088ee4716e
nzambello <nzambello@protonmail.com>
2020-07-27 10:40:11 +0200

    fix package imports

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -6,7 +6,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
-import { getSocialSettings } from 'volto-social-settings/actions/getSocialSettings';
+import { getSocialSettings } from './actions/getSocialSettings';
 import { Icon } from 'semantic-ui-react';
 import cx from 'classnames';
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { socialSettingsReducer } from 'volto-social-settings/reducers/socialSettingsReducer';
-export { default as SocialLinks } from 'volto-social-settings/components/SocialLinks/SocialLinks';
+import { socialSettingsReducer } from './reducers/socialSettingsReducer';
+export { default as SocialLinks } from './components/SocialLinks/SocialLinks';
 
 export default config => {
   config.addonReducers = {
